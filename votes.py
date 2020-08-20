7a7b6bbd514b797174c0e295a43fe3a9ea8a7775
evflores001 <67989268+evflores001@users.noreply.github.com>
2020-08-20 02:27:26 -0700

    Removed unnecessary files

--- votes.py
+++
@@ -1,8 +0,0 @@
-my_votes = int(input("How many votes did you get in the election? "))
-total_votes = int(input("What is the total votes in the election? "))
-message_to_candidate = (
-    f"You received {my_votes:,} number of votes. "
-    f"The total number of votes in the election was {total_votes:,}. "
-    f"You received {my_votes / total_votes * 100:.2f}% of the total votes. ")
-
-print(message_to_candidate)
